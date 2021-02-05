88ad495722a39df9822a39414a227780897b14ce
denizduman1 <ce.denizduman2@gmail.com>
2021-02-05 14:41:45 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            var cars = carManager.GetAll();
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Id+ " " +car.DailyPrice+ " "+car.Description);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=4,ModelYear=2006,DailyPrice=52000,Description="Temiz kullanılmış araba."},
+                new Car{Id=2,BrandId=2,ColorId=3,ModelYear=2016,DailyPrice=93000,Description="Benzini az tüketen araba."},
+                new Car{Id=3,BrandId=3,ColorId=2,ModelYear=2020,DailyPrice=140000,Description="Hızlı araba."},
+                new Car{Id=4,BrandId=4,ColorId=1,ModelYear=2009,DailyPrice=71000,Description="Fazla yıpranmamış araba."},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car deletedCar = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(deletedCar);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            Car car;
+            car = _cars.SingleOrDefault(c => c.Id == id);
+            return car;
+        }
+
+        public void Update(Car car)
+        {
+            Car updatedCar = _cars.SingleOrDefault(c => c.Id == car.Id);
+            updatedCar.ModelYear = car.ModelYear;
+            updatedCar.DailyPrice = car.DailyPrice;
+            updatedCar.ColorId = car.ColorId;
+            updatedCar.Description = car.Description;
+            updatedCar.BrandId = car.BrandId;
+        }
+    }
+}
